0d9fa9a8e41b97559ea1ae4030e0930db6517fb3
DauweQuinten <quinten.dauwe@student.howest.be>
2022-10-27 17:12:22 +0200

    feat: speed control

--- script/app.js
+++ script/app.js
@@ -1,5 +1,8 @@
-var tl = gsap.timeline({
+let animationIsPlaying = true;
+
+let tl = gsap.timeline({
   defaults: {
+    transformOrigin: 'center bottom',
     duration: 0.75,
     ease: 'power1.inOut',
   },
@@ -7,19 +10,50 @@ var tl = gsap.timeline({
   yoyo: true,
 });
 
-tl.fromTo(
-  '#Robot',
-  {
-    y: 2.5,
-  },
-  {
-    y: -2.5,
-  }
-).to(
-  '#Shadow',
-  {
-    scale: 0.75,
-    transformOrigin: 'center bottom',
-  },
-  '<'
-);
+const animationController = function () {
+  tl.fromTo(
+    '#Robot',
+    {
+      y: 2.5,
+    },
+    {
+      y: -2.5,
+    }
+  ).to(
+    '#Shadow',
+    {
+      scale: 0.75,
+    },
+    '<'
+  );
+};
+
+const listenToControls = function () {
+  const htmlPausetBtn = document.querySelector('.js-pause');
+  const htmlBtnFast = document.querySelector('.js-fast');
+  const htmlBtnNormal = document.querySelector('.js-normal');
+  const htmlBtnSlow = document.querySelector('.js-slow');
+
+  htmlPausetBtn.addEventListener('click', function () {
+    if (animationIsPlaying) {
+      animationIsPlaying = false;
+      gsap.globalTimeline.pause();
+      this.classList.add('is-paused');
+    } else {
+      animationIsPlaying = true;
+      gsap.globalTimeline.play();
+      this.classList.remove('is-paused');
+    }
+  });
+
+  // Speed control
+  htmlBtnFast.onclick = () => tl.timeScale(2);
+  htmlBtnNormal.onclick = () => tl.timeScale(1);
+  htmlBtnSlow.onclick = () => tl.timeScale(0.5);
+};
+
+addEventListener('DOMContentLoaded', function () {
+  console.info('DOM content loaded ✅');
+  animationController();
+  listenToControls();
+});
